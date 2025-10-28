d155b77ebd9bddbd1a1df7ed5ed801125f7ec302
Yaoping Wang <ypwong1992@gmail.com>
2025-10-28 16:06:16 -0400

    update graph

--- Tax_long_vs_short.py
+++ Tax_long_vs_short.py
@@ -14,7 +14,7 @@ def compare_tax_strategies(
     N=3                # 总年数
 ):
     """
-    计算短线与长线的税后收益比较，并绘图
+    计算短线与长线的税后收益比较，绘制不同短线收益率情况下的图
     """
 
     # 长线复利：持有 N 年后一次纳税
@@ -36,11 +36,52 @@ def compare_tax_strategies(
     plt.grid(True)
     plt.show()
 
+
+def compare_tax_strategies_grid(
+    t_short=0.24,      # 短期资本利得税率
+    t_long=0.15,       # 长期资本利得税率
+    g_long=0.20,       # 长期平均年化收益
+):
+    """
+    计算税后收益和长线打平所需要的短线收益率乘数，绘制不同交易频率和持有年数下的图。
+    """
+
+    k_list = np.arange(2, 25, 4)
+    N_list = np.arange(1, 10, 1)
+
+    short_to_long_ratio = np.full([len(k_list), len(N_list)], np.nan)
+
+    for i,k in enumerate(k_list):
+        # 长线复利：持有 N 年后一次纳税
+        # long_after_tax_factor = 1 + (1 - t_long) * ((1 + g_long) ** N - 1)
+        # 短线复利：每次交易后立即纳税
+        # short_after_tax_factor = (1 + r_short / k * (1 - t_short)) ** (k * N)
+        r_short = ((1 + (1 - t_long) * ((1 + g_long) ** N_list - 1))**(1/(k * N_list)) - 1) * k / (1-t_short)
+        short_to_long_ratio[i,:] = r_short #  / g_long
+
+    plt.figure(figsize=(8, 5))
+    cf = plt.imshow(short_to_long_ratio, cmap = 'viridis')
+
+    plt.colorbar(cf)
+    plt.xticks(range(len(N_list)))
+    plt.gca().set_xticklabels(N_list)
+    plt.yticks(range(len(k_list)))
+    plt.gca().set_yticklabels(k_list)
+
+    plt.xlabel('持有年数')
+    plt.ylabel('短线每年交易次数')
+    plt.title(f'短线收益率 (长线收益率={g_long}, 短线税率={t_short}, 长线税率={t_long})')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
 # 所有输入参数皆为运行假设
-compare_tax_strategies(
-    t_short=0.24,  # 短期税率
-    t_long=0.15,   # 长期税率
-    k=12,          # 年内交易次数
-    g_long=0.20,   # 长线年化
-    N=3            # 总持有年数
-)
+#compare_tax_strategies(
+#    t_short=0.24,  # 短期税率
+#    t_long=0.15,   # 长期税率
+#    k=12,          # 年内交易次数
+#    g_long=0.20,   # 长线年化
+#    N=3            # 总持有年数
+#)
+
+compare_tax_strategies_grid()
